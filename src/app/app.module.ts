5cd3af15bb896c12f5b273f563a0ea9fde596b03
Edson Moretti <edsonmoretti@live.com>
2025-11-10 15:32:42 -0300

    refactor AppModule to remove unused AppController and AppService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { AppController } from './presentation/controllers/app.controller';
 import { OAuthController } from './presentation/controllers/oauth.controller';
-import { AppService } from './application/services/app.service';
 import { GovBrPureService } from './application/services/gov-br-pure.service';
 
 @Module({
   imports: [HttpModule],
-  controllers: [AppController, OAuthController],
-  providers: [AppService, GovBrPureService],
+  controllers: [OAuthController],
+  providers: [GovBrPureService],
 })
 export class AppModule {}
